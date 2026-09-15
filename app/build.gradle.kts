59748bfd965906307ee1c59f6b522325104910fc
IU1VDW <158777227+IU1VDW@users.noreply.github.com>
2026-09-15 12:14:32 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.iu1vdw.qsolog"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
